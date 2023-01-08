96f7a9f0b3f4b1b31fc8bc399753f055f72a56e5
leaisrevolution <leaisrevolution@gmail.com>
2023-01-09 00:23:41 +0900

    [Add] Link

---
+++ nextjs-basic/nextjs-intro/components/NavBar.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function NavBar() {
+  return (
+    <nav>
+      <Link href="/">
+        <>Home</>
+      </Link>
+      <Link href="/about">
+        <>about</>
+      </Link>
+    </nav>
+  );
+}
--- nextjs-basic/nextjs-intro/pages/about.js
+++ nextjs-basic/nextjs-intro/pages/about.js
@@ -1,3 +1,10 @@
+import NavBar from '../components/NavBar';
+
 export default function about() {
-  return 'about us';
+  return (
+    <div>
+      <NavBar />
+      <h1>About us</h1>
+    </div>
+  );
 }
